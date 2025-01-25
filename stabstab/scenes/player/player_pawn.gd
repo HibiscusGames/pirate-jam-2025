494fd4a8cd68b5e05ca11aeb51652e94cf1b7d63
Hermine <irvin.dosreis@gmail.com>
2025-01-25 15:03:01 -0500

    [Trail]
    Replace Projectile Visual
    Add trail visual + script
    Pollute pawn code to setup projectile trail

--- stabstab/scenes/player/player_pawn.gd
+++ stabstab/scenes/player/player_pawn.gd
@@ -4,6 +4,9 @@ var speed: float = 500.0
 var velocity: Vector2 = Vector2.ZERO
 var decelerationRate: float = 0.1;
 
+# Trail
+var current_trail: PlayerTrail
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,11 +14,33 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if (Input.is_action_just_pressed("projectile.launch")):
 		velocity = (get_viewport().get_mouse_position() - global_position).normalized() * speed
+		make_trail()
 
 	global_translate(velocity * delta)
 	velocity -= velocity * decelerationRate * delta
-
+	
 	queue_redraw()
 
 func _draw() -> void:
 	draw_line(global_position, get_viewport().get_mouse_position(), Color.AZURE)
+
+
+# Irvin polluting your code for trail stuff sorry not sorry terry
+func make_trail() -> void:
+	if current_trail:
+		current_trail.stop()
+	current_trail = PlayerTrail.create()
+	
+	#test degueu pour changer la couleur 
+	var newTrailColor:Gradient
+	var gradient_data := {
+	0.0: Color.GREEN,
+	0.75: Color(0.0,1.0,0.0,0.75),
+	1.0: Color(1,1,1,0),
+	}
+	newTrailColor = Gradient.new()
+	newTrailColor.offsets = gradient_data.keys()
+	newTrailColor.colors = gradient_data.values()
+	current_trail.gradient = newTrailColor
+	
+	add_child(current_trail)
---
+++ stabstab/scenes/player/player_trail.gd
@@ -0,0 +1,43 @@
+class_name PlayerTrail
+extends Line2D
+
+const MAX_POINTS: int = 200
+var gradient_data := {
+	0.0: Color.DEEP_PINK,
+	0.75: Color(0.9,0.0,0.6,0.5),
+	1.0: Color(1,1,1,0),
+}
+var trail_gradient : Gradient
+
+@onready var curve := Curve2D.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	trail_gradient = Gradient.new()
+	trail_gradient.offsets = gradient_data.keys()
+	trail_gradient.colors = gradient_data.values()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#curve stuff
+	curve.add_point(get_parent().position)
+	if curve.get_baked_points().size() > MAX_POINTS:
+		curve.remove_point(0)
+	points = curve.get_baked_points()
+	
+	#color stuff
+	#self.gradient = trail_gradient
+
+#to be able to instantiate a trail
+static func create() -> PlayerTrail:
+	var scene = preload("res://scenes/player/player_trail.tscn")
+	return scene.instantiate()
+	
+# To get the remnant on stop
+func stop() -> void:
+	set_process(false)
+	var tw := get_tree().create_tween()
+	tw.tween_property(self,"modulate:a", 0.0, 1.0)
+	await tw.finished
+	queue_free()
