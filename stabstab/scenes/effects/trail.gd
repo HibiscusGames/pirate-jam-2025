b156edf6a8158551b853a546bc5c58062464da6d
pfxdev@hibiscusgames <7494211+pfouilloux@users.noreply.github.com>
2025-01-27 23:07:21 +0800

    feat(movement): refactor trail logic

---
+++ stabstab/scenes/effects/trail.gd
@@ -0,0 +1,95 @@
+class_name EffectTrail
+extends Line2D
+
+const MAX_POINTS: int = 8
+const MIN_SPAWN_DISTANCE: float = 10.0 # Minimum distance to spawn new point
+
+signal update_requested(trail: EffectTrail)
+
+####### Exported Variables #######
+
+@export var point_spawn_interval: float = 0.05 # Reduced for smoother trail
+@export var trail_length: float = 50.0 # Length of the trail in pixels
+
+
+######## Internal variables ########
+
+var _last_point: Vector2;
+var _points_positions: Array[Vector2] = []
+var _is_active: bool = true
+var _timer;
+
+######### Initialisation ##########
+
+static func create(parent: Node2D, curve: Curve, grad: Gradient) -> EffectTrail:
+	var trail = EffectTrail.new()
+	trail.z_index = parent.z_index
+	trail.width_curve = curve
+	trail.gradient = grad
+	parent.add_child(trail)
+
+	trail._last_point = parent.global_position
+	trail.add_point(parent.global_position)
+	trail._points_positions.append(parent.global_position)
+	trail._start_timer()
+
+	return trail
+
+############ Lifecycle ############
+
+func _init() -> void:
+	top_level = true
+
+func _process(_delta: float) -> void:
+	if not _is_active:
+		return
+		
+	# Update positions of existing points
+	for i in range(_points_positions.size()):
+		set_point_position(i, _points_positions[i])
+
+######## Exported functions ########
+
+func update(cur_pos: Vector2) -> void:
+	if not _is_active:
+		return
+		
+	var distance = _last_point.distance_to(cur_pos)
+	
+	# Only spawn new point if we've moved enough
+	if distance >= MIN_SPAWN_DISTANCE:
+		_points_positions.push_front(cur_pos)
+		add_point(cur_pos, 0)
+		_last_point = cur_pos
+		_update_points()
+
+func stop() -> void:
+	_is_active = false
+	_fade_out()
+	queue_free()
+
+######## Internal functions ########
+
+func _fade_out() -> void:
+	var tw := get_tree().create_tween()
+	tw.tween_property(self, "modulate:a", 0.0, 1.0)
+	await tw.finished
+
+func _start_timer() -> Timer:
+	_timer = Timer.new()
+	_timer.wait_time = point_spawn_interval
+	_timer.connect("timeout", _on_spawn_triggered)
+	add_child(_timer)
+	_timer.start()
+	return _timer
+
+func _update_points() -> void:
+	# Remove points that are too far
+	while _points_positions.size() > MAX_POINTS:
+		_points_positions.pop_back()
+		remove_point(get_point_count() - 1)
+
+######### Signal handlers ##########
+
+func _on_spawn_triggered() -> void:
+	update_requested.emit(self)
---
+++ stabstab/scenes/player/pawn.gd
@@ -0,0 +1,73 @@
+extends Node2D
+
+## Exported variables ##
+@export_group("Movement")
+@export var speed: float = 500.0
+@export var deceleration_rate: float = 0.1;
+
+@export_group("Effects")
+@export var trail_starting_point: Node2D = self
+@export var trail_width_curve: Curve
+@export var trail_color: Gradient = _new_gradient({
+	0.0: Color.GREEN,
+	0.75: Color(0.0, 1.0, 0.0, 0.75),
+	1.0: Color(1, 1, 1, 0),
+});
+
+
+@export_group("Debug")
+@export var trajectory_tracer: TrajectoryTracer2DFactory;
+
+## Internal variables ##
+
+var _velocity: Vector2 = Vector2.ZERO
+var _trajectory: Trajectory = Trajectory.create(global_position, global_position)
+var _cur_trail: EffectTrail
+
+##### Lifecycle #####
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	assert(trajectory_tracer != null, "trajectory_tracer must not be null")
+	trajectory_tracer.tracer_added.connect(_on_tracer_ready)
+
+func _process(delta: float) -> void:
+	if (not Engine.is_editor_hint()):
+		_tick(delta)
+	
+###### Signal handlers #######
+
+func _on_tracer_ready(tracer: TrajectoryTracer2D) -> void:
+	tracer._trajectory = _trajectory
+	add_child(tracer)
+
+func _on_trail_update_requested(trail: EffectTrail) -> void:
+	trail.update(trail_starting_point.global_position)
+
+######## Internal functions ########
+
+func _tick(delta: float) -> void:
+	_trajectory.end = get_local_mouse_position()
+
+	if (Input.is_action_just_pressed("projectile.launch")):
+		_velocity = _trajectory.get_direction() * speed
+		_spawn_trail()
+
+	# if (_velocity.length() < 1 && _cur_trail != null):
+	# 	_cur_trail.stop()
+
+	global_translate(_velocity * delta)
+	_velocity -= _velocity * deceleration_rate * delta
+
+func _spawn_trail():
+	if (_cur_trail == null):
+		_cur_trail = EffectTrail.create(trail_starting_point, trail_width_curve, trail_color)
+		_cur_trail.update_requested.connect(_on_trail_update_requested)
+
+func _new_gradient(data: Dictionary) -> Gradient:
+	var g = Gradient.new()
+	
+	g.offsets = data.keys()
+	g.colors = data.values()
+
+	return g
